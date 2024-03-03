397965ab9fe6f4d91d0b98a1e193faea557b6997
Devin-Jay <157307286+Devin-Jay@users.noreply.github.com>
2024-03-03 08:41:55 -0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,13 +1,11 @@
 <?php
 
-$url = getenv('mysql://rz3g4npuxheqkm4q:w16mgna61998973p@bqmayq5x95g1sgr9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/v3631j1wxf69fwg6');
-$dbparts = parse_url($url);
-
 $hostname = $dbparts['bqmayq5x95g1sgr9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	'];
 $username = $dbparts['rz3g4npuxheqkm4q']
 $password = $dbparts['w16mgna61998973p']
+$dbname = $dbparts['v3631j1wxf69fwg6']
 
-$link = mysql_connect($hostname, $username, $password);
+$link = mysql_connect($hostname, $username, $password, $dbname);
 
 if (!$link)
    {
