e57d43faff86fa1a80fa7132d4cbf0ca89af1a86
Devin-Jay <157307286+Devin-Jay@users.noreply.github.com>
2024-03-03 09:18:59 -0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -5,11 +5,11 @@ $username = $dbparts['rz3g4npuxheqkm4q']
 $password = $dbparts['w16mgna61998973p']
 $dbname = $dbparts['v3631j1wxf69fwg6']
 
-$link = mysql_connect($hostname, $username, $password, $dbname);
+$link = mysqli_connect($hostname, $username, $password, $dbname);
 
 if (!$link)
    {
-    die('Could not connect:' . mysql_error());
+    die('Could not connect:' . mysqli_error());
    }
 echo 'Connected successfully';
 ?>
