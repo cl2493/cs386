4a44fa5d8eaf8d26876761b2f5e7a647caeea5f8
Devin-Jay <157307286+Devin-Jay@users.noreply.github.com>
2024-03-19 22:20:50 -0700

    Update managerRegister.php

--- managerRegister.php
+++ managerRegister.php
@@ -9,6 +9,9 @@ try
 {
     $conn = new PDO($dsn, $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}catch(PDOException $e)
+{
+    echo "Connection failed: ". $e->getMessage();
 }
 if (isset($_POST['submit'])) {
 		//something was posted
