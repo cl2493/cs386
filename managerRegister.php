cdd84ec11204efec9a003d9980ee5a8daf046d8f
Devin-Jay <157307286+Devin-Jay@users.noreply.github.com>
2024-03-19 21:47:02 -0700

    Update managerRegister.php

--- managerRegister.php
+++ managerRegister.php
@@ -6,8 +6,8 @@ session_start();
 	if($_SERVER['REQUEST_METHOD'] == "POST")
 	{
 		//something was posted
-		$email = $_REQUEST['email'];
-		$password = $_REQUEST['password'];
+		$email = $_POST['email'];
+		$password = $_POST['password'];
 
 		if(!empty($email) && !empty($password))
 		{
