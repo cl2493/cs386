e32a9a52d4556aaec204f8e24ba3efc644180ac7
Devin-Jay <157307286+Devin-Jay@users.noreply.github.com>
2024-03-19 22:18:29 -0700

    Update managerRegister.php

--- managerRegister.php
+++ managerRegister.php
@@ -1,6 +1,15 @@
 <?php 
 session_start();
-include("connection.php");
+$dsn = 'mysql:host=bqmayq5x95g1sgr9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;port=3306;dbname=v3631j1wxf69fwg6';
+$username = 'rz3g4npuxheqkm4q';
+$password = 'w16mgna61998973p';
+
+// Create connection
+try
+{
+    $conn = new PDO($dsn, $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
 if (isset($_POST['submit'])) {
 		//something was posted
 		$email = $_POST['email'];
