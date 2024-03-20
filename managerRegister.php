55be1cd46b8b86a3b9e60b7ac7a37a41c07332c2
Devin-Jay <157307286+Devin-Jay@users.noreply.github.com>
2024-03-20 13:03:35 -0700

    trying to fix database for property owners

--- managerRegister.php
+++ managerRegister.php
@@ -18,17 +18,10 @@ if (isset($_POST['submit'])) {
 		//something was posted
 		$email = $_POST['email'];
 		$password = $_POST['password'];
-
-		if(!empty($email) && !empty($password))
-		{
-
-			//save to database
-			$query = "INSERT INTO propertyownersdb (email,password) VALUES ($email, $password)";
-			$conn->exec($query);
-		}else
-		{
-			echo "Please enter some valid information!";
-		}
+        
+        //save to database
+		$query = "INSERT INTO propertyownersdb (email,password) VALUES (?,?)";
+		$conn->prepare($query)->execute([$email],[password]);
 	}
 	$conn = null;
 ?>
