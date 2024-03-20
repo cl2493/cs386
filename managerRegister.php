1d475fda88291b342e51ac37d40a4f2d7a45c655
Devin-Jay <157307286+Devin-Jay@users.noreply.github.com>
2024-03-20 16:47:01 -0700

    edited php and html for managers

--- managerRegister.php
+++ managerRegister.php
@@ -1,53 +1,30 @@
 <?php 
 session_start();
-$dsn = 'mysql:host=bqmayq5x95g1sgr9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;port=3306;dbname=v3631j1wxf69fwg6';
-$username = 'rz3g4npuxheqkm4q';
-$password = 'w16mgna61998973p';
+include('connection.php');
 
-// Create connection
-try
-{
-    $conn = new PDO($dsn, $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}catch(PDOException $e)
-{
-    echo "Connection failed: ". $e->getMessage();
-}
-echo "help"
-if (isset($_POST['submit'])) {
-		//something was posted
+if (isset($_POST['submitBtn'])) {
+
 		$email = $_POST['email'];
 		$password = $_POST['password'];
         
         //save to database
-		$query = "INSERT INTO propertyownersdb (email,password) VALUES (?,?)";
-		$conn->prepare($query)->execute([$email],[password]);
+		$query = "INSERT INTO propertyownersdb (email,password) VALUES (:email,:password)";
+        $query_run = $conn->prepare($query);
+
+        $data =[
+            ':email' => $email,
+            ':password' => $password,
+        ];
+        $query_execute = $query_run->execute($data);
+
+        if($query_execute)
+        {
+            header('Location: index.php');
+            exit(0);
+        }
+        else {
+            header('Location: index.php');
+            exit(0);
+        }
 	}
-	$conn = null;
 ?>
-<!-----USER REGISTER PAGE------->
-<! DOCTYPE html>
-<!----STARTING SETUP------->
-<html lang="en">
-    <head>
-        <meta name ="viewport">
-        <title>Propertry Owner Portal</title>
-        <!----connecting different files-->
-        <link rel="stylesheet" href ="style/register-style.css">
-        <script src="https://kit.fontawesome.com/c011338aa2.js" crossorigin="anonymous"></script>
-    </head>
-    <!-------Start of Web Page---------->
-    <body>
-    <!------ footer ----->
-    <div class = "footer">
-        <p>Follow Us On Social Media</p>
-        <a href = "404ErrorPage.html"><i class="fa-brands fa-facebook"></i></a>
-        <a href = "404ErrorPage.html"><i class="fa-brands fa-google-plus"></i></a>
-        <a href = "404ErrorPage.html"><i class="fa-brands fa-instagram"></i></a>
-        <a href = "404ErrorPage.html"><i class="fa-brands fa-yelp"></i></a>
-        <a href = "404ErrorPage.html">Help Center</a>
-        <a href = "404ErrorPage.html">About Us</a>
-        <p>Copyright © 2024, RNT-A-ROOM</p>
-      </div>
-    </body>
-</html>
