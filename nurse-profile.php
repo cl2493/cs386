437ae4b605266f0a55152aa554abea65461166ea
Cathy Ly <cl2493@nau.edu>
2024-03-23 11:57:01 -0700

    updated with comments

--- nurse-profile.php
+++ nurse-profile.php
@@ -20,51 +20,71 @@ $verifiedFlag = true;
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- Meta tags -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Title -->
     <title>Nurse Profile</title>
+    <!-- CSS links -->
     <link rel="stylesheet" href="style/generalStyle.css">
+    <!-- JavaScript links -->
+    <!-- Font Awesome links for the Footer Icons -->
     <script src="https://kit.fontawesome.com/c011338aa2.js" crossorigin="anonymous"></script>
+    <!-- JavaScript for the navigation bar -->
     <script src="script.js" defer></script>
 </head>
+<!-- Body of the page -->
 <body>
+    <!-- Top of the page -->
     <div class="header">           
+        <!-- Navigation bar -->
         <nav id="navBar">
+            <!-- Logo -->
+            <!-- Navigation links -->
             <a href = "index.php"><img src="images/logo.png" class="logo"></a>
             <ul class="nav-links">
                 <li><a href="404ErrorPage.html">Locations</a></li>
                 <li><a href="404ErrorPage.html">Benefits</a></li>
                 <li><a href="404ErrorPage.html">Accommodation</a></li>
             </ul>
+            <!-- Profile button -->
             <a class="profile-btn" onclick="popupFunction()">Profile</a>
         </nav>
     </div>
 <!------------------------------------Profile Page------------------------------------>
     <div class="profile-container">
         <div class = "profile-content">
-
-           <div class="profile-header">
-              <h1 id="profile-Title">User Profile</h1>
-              <div id="profile-picture"></div>
-                  <?php
-                      echo "<h2 class='name-info'><strong>$first_name $middle_name $last_name";
-                      if ($verifiedFlag) {
-                          echo "<img src='images/icons/check-symbol.png' class='verified-icon'>";
-                      }
-                      echo "</strong></h2>";
-                   ?>
-           </div>
+            <!-- Profile header -->
+            <div class="profile-header">
+                <!-- Profile picture -->
+                <h1 id="profile-Title">User Profile</h1>
+                <!-- Profile name -->
+                <div id="profile-picture"></div>
+                <!-- Profile name -->
+                <?php
+                // Display the name of the user
+                echo "<h2 class='name-info'><strong>$first_name $middle_name $last_name";
+                // Display the verified icon if the user is verified
+                if ($verifiedFlag) {
+                    echo "<img src='images/icons/check-symbol.png' class='verified-icon'>";
+                }
+                // Close the strong tag
+                echo "</strong></h2>";
+                ?>
+            </div>
+            <!-- Profile data -->
            <div class="profile-data">
                <!-- Move the form and profile picture div here -->
                <!--on the left will have tabs for payment to update the screen -->
                <div class = "profile-nav">
                <div class="profile-tabs">
                    <li ><a href="nurse-profile.php" class="active">Profile</a></li>
-                   <li><a href="nurse-availability.php">Payment</a></li>
+                   <!-- These links will be updated to the correct pages -->
+                   <li><a href="">Payment</a></li>
                    <li><a href = "" >History</a></li>
                    <li><a href="">Settings</a></li>
                </div>
             </div>
-
+            <!-- Profile information -->
             <div class="profile-info">
                 <div class='space-top'></div>
                 <p><strong>User ID:</strong> <?php echo $userID; ?></p>
