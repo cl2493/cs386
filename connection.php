ec3d0e8ecc798c7d6c972a8b425e08ef7893e796
Devin-Jay <157307286+Devin-Jay@users.noreply.github.com>
2024-03-24 09:36:37 -0700

    deleted my test code for database

--- connection.php
+++ connection.php
@@ -3,9 +3,6 @@
 $dsn = 'mysql:host=bqmayq5x95g1sgr9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;port=3306;dbname=v3631j1wxf69fwg6';
 $username = 'rz3g4npuxheqkm4q';
 $password = 'w16mgna61998973p';
-$dsn = 'mysql:host=localhost;dbname=testdb';
-$username = 'root';
-$password = '';
 
 // Create connection
 try
