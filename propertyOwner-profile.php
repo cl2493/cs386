a71f44b1b190de3cd0b24a4229ebec3003e6bc25
Arh15 <102483879+Arh15@users.noreply.github.com>
2024-04-13 20:01:15 -0700

    Updated Template for listed Properties
    -fixed star animation
    -cleaned up code
    -cleaned up layout

--- propertyOwner-profile.php
+++ propertyOwner-profile.php
@@ -208,7 +208,7 @@ function newMessageIcon($newMessageFlag)
                        <h3 class='property-bed'>Beds: <?=$listings[$listing]->bed?></h3>
                        <h3 class='property-bath'>Baths: <?=$listings[$listing]->bath?></h3>
                        <h3 class='property-rent'><?=$listings[$listing]->price?></h3>
-                    <a class="property-btn" href = "#">View Property</a>
+                    <a class="property-btn" href = "ListedPropertyTemp">View Property</a>
                 </div>
             </div>
                 <?php
