e05a6eb90dce680659bd83a1f359ed235c4fa754
Devin-Jay <dds324@nau.edu>
2024-04-15 19:55:01 -0700

    Edited filter form

--- directorySearch.php
+++ directorySearch.php
@@ -5,29 +5,49 @@
     <div class = "search-container">
         <div class = "search-content">
             <div class = "location-search">
-                <form>
+                <form action="filter.php" method="post">
                     <div class="search-input">
                         <label>Location</label>
-                        <input type="text" placeholder="New York, Chicago...">
+                        <input name="location" type="text" placeholder="New York, Chicago..." value="<?=$location?>">
                     </div>
                     <div class="content-dropdown" data-dropdown>
                         <label># of Beds</label>
-                        <button class="drop" data-dropdown-button>Beds</button>
-                        <div class="dropdown-menu">
-                            <div class= "dropdown-menu-selection">
-                                <a href="#" class = "link">1</a>
-                                <a href="#" class = "link">2</a>
-                                <a href="#" class = "link">3</a>
-                                <a href="#" class = "link">4</a>
-                                <a href="#" class = "link">5</a>
-                            </div>
-                        </div>
+                        <select name="bed" id="" class="drop">
+                            <option value=""><?=$bed?></option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                        </select>
                     </div>
                     <div class="content-select">
                         <label>Lease Length</label>
                         <input type="text" id="lease_length" placeholder="Select lease length">
                     </div>
-                    <button class="sub-btn" type="submit"><img src = "assets/search.png" alt="serach icon"></button>
+                    <div class="price-input-container"> 
+                        <div class="price-input"> 
+                            <div class="price-field"> 
+                                <span>Minimum Price</span> 
+                                <input name="minPrice" type="number" class="min-input" value="0"> 
+                            </div> 
+                            <div class="price-field"> 
+                                <span>Maximum Price</span> 
+                                <input name="maxPrice" type="number" class="max-input" value="10000"> 
+                            </div> 
+                        </div> 
+                    <div class="slider-container"> 
+                        <div class="price-slider"> 
+                        </div> 
+                    </div> 
+                </div> 
+  
+            <!-- Slider -->
+            <div class="range-input"> 
+                <input type="range" class="min-range" min="0" max="10000" value="0" step="500"> 
+                <input type="range" class="max-range" min="0" max="10000" value="10000" step="500"> 
+            </div>
+                    <button name="filterBtn" class="sub-btn" type="submit"><img src = "assets/search.png" alt="serach icon"></button>
                 </form>
             </div>
         </div>
