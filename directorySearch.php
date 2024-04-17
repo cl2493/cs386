db393888c8260a684d52957681aab1a00f595b56
Cathy Ly <cl2493@nau.edu>
2024-04-16 19:50:19 -0700

    It's not done.. please be patent

--- directorySearch.php
+++ directorySearch.php
@@ -1,10 +1,8 @@
-    <!-- jQuery UI library -->
-    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-
+<link rel="stylesheet" href="style/listing-filter.css">
+<!--- Listing Search Filter --->
 <div class="container">
     <div class = "search-container">
         <div class = "search-content">
-            <div class = "location-search">
                 <form action="filter.php" method="post">
                     <div class="search-input">
                         <label>Location</label>
@@ -21,9 +19,22 @@
                             <option value="5">5</option>
                         </select>
                     </div>
+                    <div class="content-dropdown" data-dropdown>
+                        <label># of Baths</label>
+                        <select name="baths" id="" class="drop">
+                            <option value="">Baths</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                        </select>
+                    </div>
                     <div class="content-select">
-                        <label>Lease Length</label>
-                        <input type="text" id="lease_length" placeholder="Select lease length">
+                        <label>Lease Start</label>
+                        <input type="date" id="lease_length" placeholder="Select lease length">
+                        <label>Lease End</label>
+                        <input type="date" id="lease_length" placeholder="Select lease length">
                     </div>
                     <div class="price-input-container"> 
                         <div class="price-input"> 
@@ -37,35 +48,16 @@
                             </div> 
                         </div> 
                     <div class="slider-container"> 
-                        <div class="price-slider"> 
-                        </div> 
+                        <div class="price-slider"> </div> 
                     </div> 
                 </div> 
-  
-            <!-- Slider -->
-            <div class="range-input"> 
-                <input type="range" class="min-range" min="0" max="10000" value="0" step="500"> 
-                <input type="range" class="max-range" min="0" max="10000" value="10000" step="500"> 
-            </div>
+                <!-- Slider -->
+                <div class="range-input"> 
+                    <input type="range" class="min-range" min="0" max="10000" value="0" step="500"> 
+                    <input type="range" class="max-range" min="0" max="10000" value="10000" step="500"> 
+                </div>
                     <button name="filterBtn" class="sub-btn" type="submit"><img src = "assets/search.png" alt="serach icon"></button>
                 </form>
             </div>
-        </div>
     </div>
 </div>
-
-<script>
-    $(function() {
-        $("#lease_length").datepicker({
-            numberOfMonths: 2, // Display two months in the datepicker
-            showButtonPanel: true, // Show a button panel with 'Today' and 'Done' buttons
-            dateFormat: "yy-mm-dd", // Date format
-            onSelect: function(selectedDate) {
-                var endDate = $(this).datepicker("getDate"); // Get selected date
-                endDate.setDate(endDate.getDate() + 30); // Add 30 days to selected date
-                $("#lease_length").datepicker("option", "maxDate", endDate); // Set max date
-            }
-        });
-    });
-</script>
-
