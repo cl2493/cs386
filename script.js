b2605896b78b22c6dff8b7150a5c5fd41cefe426
Cathy Ly <cl2493@nau.edu>
2024-04-26 22:04:45 -0700

    uwuwuwuwwu

--- script.js
+++ script.js
@@ -75,100 +75,66 @@ function showMessageFunction() {
     alert("You need to be logged in to view a property.")
 }
 
-// price range stuff
-const rangevalue =  
-    document.querySelector(".slider-container .price-slider"); 
-const rangeInputvalue =  
-    document.querySelectorAll(".range-input input"); 
-  
-// Set the price gap 
-let priceGap = 500; 
-  
-// Adding event listners to price input elements 
-const priceInputvalue =  
-    document.querySelectorAll(".price-input input"); 
-for (let i = 0; i < priceInputvalue.length; i++) { 
-    priceInputvalue[i].addEventListener("input", e => { 
-  
-        // Parse min and max values of the range input 
-        let minp = parseInt(priceInputvalue[0].value); 
-        let maxp = parseInt(priceInputvalue[1].value); 
-        let diff = maxp - minp 
-  
-        if (minp < 0) { 
-            alert("minimum price cannot be less than 0"); 
-            priceInputvalue[0].value = 0; 
-            minp = 0; 
-        } 
-  
-        // Validate the input values 
-        if (maxp > 10000) { 
-            alert("maximum price cannot be greater than 10000"); 
-            priceInputvalue[1].value = 10000; 
-            maxp = 10000; 
-        } 
-  
-        if (minp > maxp - priceGap) { 
-            priceInputvalue[0].value = maxp - priceGap; 
-            minp = maxp - priceGap; 
-  
-            if (minp < 0) { 
-                priceInputvalue[0].value = 0; 
-                minp = 0; 
-            } 
-        } 
-  
-        // Check if the price gap is met  
-        // and max price is within the range 
-        if (diff >= priceGap && maxp <= rangeInputvalue[1].max) { 
-            if (e.target.className === "min-input") { 
-                rangeInputvalue[0].value = minp; 
-                let value1 = rangeInputvalue[0].max; 
-                rangevalue.style.left = `${(minp / value1) * 100}%`; 
-            } 
-            else { 
-                rangeInputvalue[1].value = maxp; 
-                let value2 = rangeInputvalue[1].max; 
-                rangevalue.style.right =  
-                    `${100 - (maxp / value2) * 100}%`; 
-            } 
-        } 
-    }); 
-  
-    // Add event listeners to range input elements 
-    for (let i = 0; i < rangeInputvalue.length; i++) { 
-        rangeInputvalue[i].addEventListener("input", e => { 
-            let minVal =  
-                parseInt(rangeInputvalue[0].value); 
-            let maxVal =  
-                parseInt(rangeInputvalue[1].value); 
-  
-            let diff = maxVal - minVal 
-              
-            // Check if the price gap is exceeded 
-            if (diff < priceGap) { 
-              
-                // Check if the input is the min range input 
-                if (e.target.className === "min-range") { 
-                    rangeInputvalue[0].value = maxVal - priceGap; 
-                } 
-                else { 
-                    rangeInputvalue[1].value = minVal + priceGap; 
-                } 
-            } 
-            else { 
-              
-                // Update price inputs and range progress 
-                priceInputvalue[0].value = minVal; 
-                priceInputvalue[1].value = maxVal; 
-                rangevalue.style.left = 
-                    `${(minVal / rangeInputvalue[0].max) * 100}%`; 
-                rangevalue.style.right = 
-                    `${100 - (maxVal / rangeInputvalue[1].max) * 100}%`; 
-            } 
-        }); 
-    } 
-}
+    // JavaScript for the price range slider
+    // Get the range input and price input
+    const rangeInput = document.querySelectorAll('.range-input input');
+    priceInput = document.querySelectorAll('.price-input-container input');
+    // Get the progress bar
+    progress = document.querySelector(".slider .price");
+    // Set the price gap
+    let priceGap = 1000;
+
+    priceInput.forEach(input =>{
+        // Add event listener to the input
+        input.addEventListener("input", e =>{
+            //get the two input values of the range input
+            let minVal = parseInt(priceInput[0].value);
+            maxVal = parseInt(priceInput[1].value);
+            //get the two values of the range input
+            if((maxVal - minVal >= priceGap) && max <= 5000 && min >= 0){
+                // Set the value of the range input
+                if (e.target.className === "min-input")
+                {
+                    // Set the value of the range input
+                    rangeInput[0].value = minVal;
+                    progress.style.left = (minVal / rangeInput[0].max) * 90 + "%";
+                }
+                else{
+                    rangeInput[1].value = maxVal;
+                    progress.style.right = 100 - (maxVal / rangeInput[1].max) * 90 + "%";
+                }
+            }
+        })
+    })
+
+    // Add event listener to the range input
+    rangeInput.forEach(input =>{
+        // Add event listener to the input
+        input.addEventListener("input", e =>{
+            //get the two values of the range input
+            let minVal = parseInt(rangeInput[0].value);
+            maxVal = parseInt(rangeInput[1].value);
+            //get the two values of the price input
+            if(maxVal - minVal < priceGap){
+                // Set the value of the price input
+                if (e.target.className === "range-min")
+                {
+                    // Set the value of the price input
+                    rangeInput[0].value = maxVal - priceGap;
+                }
+                else{
+                    rangeInput[1].value = minVal + priceGap;
+                }
+            }
+            else{
+                // Set the value of the price input
+                priceInput[0].value = minVal;
+                priceInput[1].value = maxVal;
+                progress.style.left = (minVal / rangeInput[0].max) * 100 + "%";
+                progress.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+            }
+        })
+    })
 
 // function to hide certification upload section after upload
 window.addEventListener('DOMContentLoaded', function () 
