f5a93cc675e245f1eccd93fbd28aa08ada54994f
Cathy Ly <cl2493@nau.edu>
2024-04-27 17:15:43 -0700

    added settings and updated header and nurse-profil

---
+++ settings.php
@@ -0,0 +1,38 @@
+<!-- Nurse profile header -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Meta tags -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Title -->
+    <title>Nurse Profile</title>
+    <!-- CSS links -->
+    <link rel="stylesheet" href="style/generalStyle.css">
+    <!-- JavaScript links -->
+    <!-- Font Awesome links for the Footer Icons -->
+    <script src="https://kit.fontawesome.com/c011338aa2.js" crossorigin="anonymous"></script>
+    <!-- JavaScript for the navigation bar -->
+    <script src="script.js" defer></script>
+</head>
+<!-- Body of the page -->
+<body>
+    <!-- Top of the page -->
+    <?php include('header.php'); ?>
+    <form>
+        <!--  Allows editing on the profile page. -->
+        
+
+
+    </form>
+    <form id="certForm" action="upload_certification.php" method="post" enctype="multipart/form-data">
+        <div class="cert-upload">
+            <h2>Upload Certification</h2>
+        </div>
+        <label for="certFile" class="select-btn">Select PDF File</label>
+        <input type="file" id="certFile" name="certFile" accept=".pdf" class="file-input">
+        <button type="submit" class="upload-btn">Upload</button>
+        <span id='fileNameDisplay' class='file-name-display'></span>
+    </form>
+</body>
+</html>
+
